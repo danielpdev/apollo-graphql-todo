276654f6d8d205069757400bc23608edd57111f7
Glitch (shadow-nest) <none>
2019-01-31 08:58:17 +0000

    😄😘 Checkpoint ./server.js:2988573/6

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const schema = require('./schema');
 const path = require('path');
-// const cors = require('cors')
+const cors = require('cors')
 const app = express();
-// app.use(cors())
+app.use(cors())
 // Replace with your mongoLab URI
 // mongodb://<dbuser>:<dbpassword>@ds247101.mlab.com:47101/todos-apollo-graphql
 const MONGO_URI = 'mongodb://admin:password1@ds247101.mlab.com:47101/todos-apollo-graphql';
