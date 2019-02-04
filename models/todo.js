2d588cd6ad2493e12714a95d502347244e47176b
Glitch (shadow-nest) <none>
2019-02-04 12:39:21 +0000

    🏅🍟 Checkpoint ./models/todo.js:2988573/334

--- models/todo.js
+++ models/todo.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'); //require mongoose for defining our todo schema
 const Schema = mongoose.Schema;
 
 const TodoSchema = new Schema({
-  likes: { type: Number, default: 0 },
+  likes: { type: Number, default: 0 }, //default 0 becuase when we first define a todo we have need to have 0 likes
   content: { type: String }
 });
 
-TodoSchema.statics.like = function(id) {
+TodoSchema.statics.like = function(id) { // add a function for setting like in order to call TodoSchema.like(id) 
   const Todo = mongoose.model('todo');
 
   return Todo.findById(id)
