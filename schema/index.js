cc6b060dcaa3fe940651ffe97ef1909f20a8e630
Glitch (shadow-nest) <none>
2019-02-04 05:48:38 +0000

    🐥📍 Checkpoint ./server.js:2988573/113 ./schema/index.js:2988573/29 ./package.json:2988573/1471

--- schema/index.js
+++ schema/index.js
@@ -1,4 +1,3 @@
-const _ = require('lodash');
 const graphql = require('graphql');
 const { GraphQLSchema } = graphql;
 
--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const cors = require('cors')
 const app = express();
 app.use(cors())
 // Replace with your mongoLab URI
-// mongodb://<dbuser>:<dbpassword>@ds247101.mlab.com:47101/todos-apollo-graphql
 const MONGO_URI = 'mongodb://admin:password1@ds247101.mlab.com:47101/todos-apollo-graphql';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
@@ -28,7 +27,5 @@ app.use('/graphql', expressGraphQL({
 }));
 
 app.listen(4000, () => {
-  console.log('Listening');
+  console.log('Listening at 4000');
 });
-
-module.exports = app;
